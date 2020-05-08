ea69b054396121221f512f5b73518d43eb672cc2
Elen Carvalho <60263754+Elen0207@users.noreply.github.com>
2020-05-08 20:57:51 -0300

    Update exgo.go

--- exgo.go
+++ exgo.go
@@ -1,33 +1,30 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-)//biblioteca
-//0 , 999999999999
-func geraVetor()[100]int{
-    var vetor [100]int
+	"fmt"
+	"math/rand"
+)
 
-    for i := 0 ; i < 100 ; i++ {  //:= significa que o valor ainda não foi definido
-        vetor[i]  = int((rand.Float32() * 1000) + 1)  // parse para ,
-    }
-    return vetor
-}
-func  main(){
-    var vet [100]int
-    var qtdPares, qtdImpares int
+func main() {
+	var vetor [100]int
+	var contPares, contImpares int
 
-    vet = geraVetor()
-    fmt.Println(vet)
+	for i := 0; i < 100; i++ {
+		vetor[i] = int((rand.Float32() * 1000) + 1)
+	}
+	fmt.Println(vetor)
 
-    for i := 0 ; i < 100 ; i++ {
-        if vet[i] % 2 ==0 {
-            qtdPares++
-        } else{
-            qtdImpares
-        }
-    }
-    fmt.Println("Total pares: ", qtdPares)
-	fmt.Println("Total impares: ", qtdImpares)
+	for i := 0; i < 100; i++ {
+
+		if vetor[i]%2 == 0 {
+			contPares++
+		} else {
+			contImpares++
+		}
+
+	}
+
+	fmt.Println("Total pares: ", contPares)
+	fmt.Println("Total impares: ", contImpares)
 
 }
